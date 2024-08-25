53069e09ccbd7c92eefbb22df7e327cca17a7c84
xlebedx <xlebedx@gmail.com>
2024-08-25 12:52:05 +0300

    1

--- module_2_2.py
+++ module_2_2.py
@@ -1,10 +1,10 @@
-first = int(input('Ведите число 1: '))
-second = int(input('Введите число 2: '))
-third = int(input('Введите число 3: '))
+first = (input('Ведите число 1: '))
+second = (input('Введите число 2: '))
+third = (input('Введите число 3: '))
 if first == second == third:
     print(3)
 elif first == second or second == third or first == third:
     print(2)
-elif first != second and second != third and first != third:
+else:
     print(0)
 #commit
