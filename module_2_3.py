5aa73f3b1403030e21a7cd0cb4df0baefb53c371
xlebedx <xlebedx@gmail.com>
2024-08-27 01:01:58 +0300

    все переделал

--- module_2_3.py
+++ module_2_3.py
@@ -1,9 +1,9 @@
 my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
-my_list_beginning_index = 0
-while my_list[my_list_beginning_index] >= len(my_list) and my_list[my_list_beginning_index] > 0:
-    print(my_list[my_list_beginning_index])
-    my_list_beginning_index += 1
-    if my_list[my_list_beginning_index] == 0:
-        my_list_beginning_index += 1
-    if my_list[my_list_beginning_index] < 0:
+i = 0
+while i < len(my_list):
+    if my_list[i] > 0:
+        print(my_list[i])
+    if my_list[i] < 0:
         break
+    i += 1
+
