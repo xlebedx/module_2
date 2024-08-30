f3c541fed2917a17983ec8a6403a0b38e8a098b0
xlebedx <xlebedx@gmail.com>
2024-08-30 15:14:42 +0300

    11

--- module_2_hard.py
+++ module_2_hard.py
@@ -1,15 +1,11 @@
 import random
-
 first_board = [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
 second_board = []
-n = 4 # (random.choice(first_board))
+n =  random.choice(first_board)
 print(n)
-for j in range(1, int(n/2 + 1)):
-    for k in range(1, n + 1):
-        if j + k > n + 1:
-            continue
-        elif n % (j + k) == 0:
-            second_board.append(j)
-            second_board.append(k)
-result = ("".join(map(str, second_board)))
-print(result)
+for j in range(1, n):
+    for k in range(j+1, n):
+       if n % (j + k) == 0:
+            second_board += str(j) + str(k)
+password = ("".join(map(str, second_board)))
+print(password)
