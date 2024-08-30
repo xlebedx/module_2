0d8ad27bde182d78621e8286e014f0168a956c90
xlebedx <xlebedx@gmail.com>
2024-08-30 04:36:57 +0300

    !

---
+++ module_2_hard.py
@@ -0,0 +1,15 @@
+import random
+
+first_board = [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+second_board = []
+n = (random.choice(first_board))
+print(n)
+for j in range(1,n+1):
+        for k in range(1,n+1):
+            if j+k > n+1:
+                 continue
+            if n%(j + k) == 0:
+                second_board.append(j)
+                second_board.append(k)
+result = ("".join(map(str,second_board)))
+print(result)
